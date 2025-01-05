a2b563dbc8f0a1bcd12fa3170aa3a9ace3229262
Serhii Dubetskyi <serhiidubetskyi@pm.me>
2025-01-05 19:58:37 +0100

    Fix root variable reassignment in sphere intersection calculation

--- src/hittable/sphere.rs
+++ src/hittable/sphere.rs
@@ -34,9 +34,9 @@ impl Hittable for Sphere {
         }
 
         let discriminant_sqrt = discriminant.sqrt();
-        let root = (h - discriminant_sqrt) / a;
+        let mut root = (h - discriminant_sqrt) / a;
         if !ray_t.surrounds(&root) {
-            let root = (h + discriminant_sqrt) / a;
+            root = (h + discriminant_sqrt) / a;
             if !ray_t.surrounds(&root) {
                 return None;
             }
